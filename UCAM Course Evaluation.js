42663835b1bac608350232617bfb02dff496125b
Pavel Rahman <52383535+pavstar619@users.noreply.github.com>
2021-05-18 12:22:35 +0600

    Update UCAM Course Evaluation.js

--- UCAM Course Evaluation.js
+++ UCAM Course Evaluation.js
@@ -1,6 +1,6 @@
 var i;
 var x=1;
 for(i = 1; i<17;i++){
-    document.getElementById('ctl00_MainContainer_rbtTheory'+x+'_0').checked = true;
+    document.getElementById('ctl00_MainContainer_rbtTheory'+x+'_0').checked = true; // change to rbtLab for labs
     x++;
 }
